51e1dd0d52c8cf19af0b7773a03d92dde83b27a5
timsmithxyz <smitht38@tcnj.edu>
2017-02-04 22:14:25 -0500

    add checking from external list

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,12 +2,21 @@ chrome.extension.sendMessage({}, function (response) {
     var readyStateCheckInterval = setInterval(function () {
         if (document.readyState === "complete") {
             clearInterval(readyStateCheckInterval)
-
-            const badSites = ['100percentfedup.com']
-
-            if (badSites.includes(document.URL.split('/')[2])) {
-              alert("Hold up there, friend. This site is known to use Alternative Facts™, so you probably shouldn't listen to anything you read here.")
-            }
+			let sites = "";
+			let list = [];
+			let xhr = new XMLHttpRequest()
+			xhr.open('GET', 'http://timsmith.xyz/sites.txt', true)
+    		xhr.onload = () => {
+      			if (xhr.status >= 200 && xhr.status < 400) {
+        			sites = xhr.response;
+					console.log(xhr.response);
+					list = sites.split(/\n/);
+					if (sites.includes(document.URL.split('/')[2])) {
+              			alert("Hold up there, friend. This site is known to use Alternative Facts™, so you probably shouldn't listen to anything you read here.")
+            		}
+      			}
+    		}
+    		xhr.send()            
 
         }
     }, 10)
